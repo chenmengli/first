148c648ccb031713949105f39ed92667a46382b5
chenmengli <cml0930@126.com>
2020-04-30 23:09:38 +0800

    1.0.0

---
+++ src/main/java/com/ceshi/Login.java
@@ -0,0 +1,19 @@
+/*
+* Login.java
+* Version 1.0.0
+* Created on 2020年4月30日
+* Copyright cml
+*
+*/
+package com.ceshi;
+
+/**   
+ * 项目名称：first-project   
+ * 类名称：Login   
+ * 类描述：   
+ * 创建人：cml   
+ * 创建时间：2020年4月30日 下午11:04:38        
+ */
+public class Login {
+
+}
